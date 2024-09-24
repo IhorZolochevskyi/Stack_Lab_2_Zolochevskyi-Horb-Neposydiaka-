2e53644f31229777a81b5ab3a34e4c2d9cd8d6b2
Ігор Золочевський <Ihor.Zolochevskyi@cs.khpi.edu.ua>
2024-09-24 13:14:23 +0300

    Добавлены массивы и списки, вывод элементов массива A
    В метод `Main` класса `Program` добавлены массивы и списки:
    - Массив строк `A` (названия игр)
    - Массив целых чисел `B`
    - Массив строк `C` (названия цветов)
    - Список строк `myCars` (названия автомобилей)
    - Список строк `yourCars` (названия автомобилей)
    
    Добавлен вывод элементов массива `A`:
    - В прямом порядке с использованием цикла `foreach`
    - В обратном порядке с использованием цикла `for` и метода `Reverse()`
    - В обратном порядке с использованием цикла `foreach`

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -7,6 +7,34 @@ namespace Lab_2
         static void Main(string[] args)
         {
             Car car = new Car(123, "жовтий", "audi", 5); 
+
+            String[] A= { "Falluot 3", "Daxter 2", "System Shok 2", "Morrowind", "Pes 2013" };
+
+            int[] B = { 2, -7, -10, 6, 7, 9, 3 };
+
+            String[] C = { "Light Green", "Red", "Green", "Yellow", "Purple", "Dark Green", 
+                "Light Red", "Dark Red", "Dark Yellow", "Light Yellow" };
+
+            List<string> myCars = new List<String> { "Yugo", "Aztec", "BMW" };
+
+            List<string> yourCars = new List<String> { "BMW", "Saab", "Aztec" };
+
+            Console.WriteLine("Масив A в по порядку: ");
+            foreach (string i in A)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(A[i].Reverse());
+            }
+
+            Console.WriteLine("\nМасив A в зворотньому порядку: ");
+            foreach (string i in A)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
